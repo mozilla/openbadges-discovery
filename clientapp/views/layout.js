c0183a04db8f0d086c08a4f42da9e45605cfcdb4
Mike Larsson <mikelarssonftw@gmail.com>
2014-03-05 17:03:10 -0500

    Lose top bar for hamburger menu

--- clientapp/views/layout.js
+++ clientapp/views/layout.js
@@ -5,7 +5,8 @@ module.exports = HumanView.extend({
   template: templates.layout,
   events: {
     'click .js-login': 'login',
-    'click .js-logout': 'logout'
+    'click .js-logout': 'logout',
+    'click .js-view-latest': 'landing'
   },
   render: function () {
     this.renderAndBind(this.model);
@@ -27,5 +28,11 @@ module.exports = HumanView.extend({
   logout: function (e) {
     this.model.logout();
     e.preventDefault();
+  },
+  landing: function (evt) {
+    window.app.router.navigateTo('/');
+    // TODO: generalize the following to all(?) dropdown buttons
+    var dropdown = $(evt.currentTarget).closest('.f-dropdown');
+    Foundation.libs.dropdown.close(dropdown);
   }
 });
