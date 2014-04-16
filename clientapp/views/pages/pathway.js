776d32f5b60501400789006ba0fe5d9e394194b6
Mike Larsson <mikelarssonftw@gmail.com>
2014-04-16 13:43:15 -0400

    Remove stray console.logs

--- clientapp/views/pages/pathway.js
+++ clientapp/views/pages/pathway.js
@@ -9,7 +9,6 @@ module.exports = HumanView.extend({
       pathway: this.model,
       user: window.app.currentUser
     });
-    console.log(this.model);
     this.renderSubview(new Editor({
       collection: this.collection,
       mode: 'view'
