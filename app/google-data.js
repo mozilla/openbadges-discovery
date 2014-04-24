1864f8addac365ce3b5241faa00ac82bdb2f7897
Mike Larsson <mikelarssonftw@gmail.com>
2014-04-23 20:59:25 -0400

    Fetch from all worksheets and sort badges with images to the top

--- app/google-data.js
+++ app/google-data.js
@@ -1,6 +1,7 @@
 const GoogleClientLogin = require("googleclientlogin").GoogleClientLogin;
 const GoogleSpreadsheets = require("google-spreadsheets");
 const config = require('./lib/config');
+const async = require('async');
 
 function log () { if (config('DEV', false)) console.log.apply(null, arguments); }
 
@@ -17,28 +18,39 @@ module.exports = function load (cb) {
           key: config('GOOGLE_KEY'),
           auth: googleAuth.getAuthId()
       }, function(err, spreadsheet) {
-          var rowCount = spreadsheet.worksheets[0].rowCount;
-          log('Worksheet row count: %d', rowCount);
-          spreadsheet.worksheets[0].rows({
-            start: 1,
-            num: rowCount
-          }, function(err, cells) {
-            log('Fetched %d cells', cells.length);
-            var time = Date.now();
-            var achievements = cells.map(function (cell) {
-              var imgSrc = (typeof cell.imagefile === 'string') ? cell.imagefile : '';
-              imgSrc = imgSrc.replace('www.dropbox.com', 'dl.dropboxusercontent.com');
-              return {
-                created_at: time--,
-                type: 'badge',
-                title: (typeof cell.badgename === 'string') ? cell.badgename : "No title in " + cell.title,
-                description: typeof cell.description === 'string' ? cell.description : "No description in " + cell.title,
-                tags: (typeof cell.tags === 'string') ? cell.tags.split(/[, ]+/) : [],
-                creator: "A. Creator",
-                imgSrc: imgSrc
-              };
+          async.concat(spreadsheet.worksheets, function (worksheet, cb) {
+            log('Worksheet %s row count: %d', worksheet.title, worksheet.rowCount);
+            worksheet.rows({
+              start: 1,
+              num: worksheet.rowCount
+            }, function(err, cells) {
+              log('Fetched %d cells', cells.length);
+              var achievements = cells.map(function (cell) {
+                var imgSrc = (typeof cell.imagefile === 'string') ? cell.imagefile : '';
+                imgSrc = imgSrc.replace('www.dropbox.com', 'dl.dropboxusercontent.com');
+                return {
+                  type: 'badge',
+                  title: (typeof cell.badgename === 'string') ? cell.badgename : "No title in " + cell.title,
+                  description: typeof cell.description === 'string' ? cell.description : "No description in " + cell.title,
+                  tags: (typeof cell.tags === 'string') ? cell.tags.split(/[, ]+/) : [],
+                  creator: "A. Creator",
+                  imgSrc: imgSrc
+                };
+              });
+              cb(null, achievements);
             });
-            cb({achievements: achievements});
+          }, function (err, achievements) {
+            if (err) throw err;
+            achievements.sort(function (a, b) {
+              if ((a.imgSrc && b.imgSrc) || (!a.imgSrc && !b.imgSrc)) return 0;
+              if (a.imgSrc) return -1;
+              return 1;
+            });
+            var time = Date.now();
+            achievements.forEach(function (achievement) {
+              achievement.created_at = time--;
+            });
+            return cb({achievements: achievements});
           });
       });
   });
